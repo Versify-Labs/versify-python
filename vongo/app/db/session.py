84c8cbe34b4aa39984b42054b27ca4d79ba0bf0c
Troy Smith <troywsmith123@gmail.com>
2023-01-06 18:13:18 -0500

    Update session.py

--- vongo/app/db/session.py
+++ vongo/app/db/session.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 from pymongo.collection import Collection
 from pymongo.database import Database
-
+from typing import Optional
 from ..core.config import settings
 
 
@@ -20,17 +20,42 @@ class SessionLocal:
             connection_str = f"mongodb+srv://{mongo_user}:{mongo_password}@{mongo_domain}/?retryWrites=true&w=majority"
 
         try:
-            self.cluster = MongoClient(connection_str)
+            self._client = MongoClient(connection_str)
             print("Connected to MongoDB")
         except Exception as e:
             print(e)
             print("Could not connect to MongoDB")
 
-    def get_db(self, db_name: str) -> Database:
-        return self.cluster[db_name]
+    @property
+    def client(self) -> MongoClient:
 
-    def get_collection(self, db_name: str, collection_name: str) -> Collection:
-        return self.cluster[db_name][collection_name]
+        if self._client is None:
+            raise ValueError("Database not initialized")
+
+        return self._client
+
+    def get_db(self, db_name: str) -> Optional[Database]:
+
+        if not self.client:
+            return None
+
+        return self.client[db_name]
+
+    def get_collection(
+        self, db_name: str, collection_name: str
+    ) -> Optional[Collection]:
+
+        if not self.client:
+            return None
+
+        if db_name not in self.client.list_database_names():
+            return None
+
+        db = self.client[db_name]
+        if collection_name not in db.list_collection_names():
+            return None
+
+        return db[collection_name]
 
     def close(self) -> None:
-        self.cluster.close()
+        self.client.close()
