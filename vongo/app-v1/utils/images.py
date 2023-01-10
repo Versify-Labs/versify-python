1ce92d2d5177616210e6e517b0d5b8d94a10070f
Troy Smith <troywsmith123@gmail.com>
2023-01-10 11:15:34 -0500

    clean

--- vongo/app-v1/utils/images.py
+++
@@ -1,8 +0,0 @@
-from ..constants import DEFAULT_LOGO
-
-
-def get_image(name):
-    if name:
-        return f"https://avatars.dicebear.com/api/initials/{name[0]}.svg"
-    else:
-        return DEFAULT_LOGO
